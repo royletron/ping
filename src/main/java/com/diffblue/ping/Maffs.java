4029d89ce4c9b22b5634886079a15742569ffe1d
Darren Royle <darren@diffblue.com>
2017-09-18 15:23:06 +0100

    Say hello to maffs

--- src/main/java/com/diffblue/ping/Maffs.java
+++ src/main/java/com/diffblue/ping/Maffs.java
@@ -4,4 +4,17 @@ package com.diffblue.ping;
  * Created by darrenroyle on 18/09/2017.
  */
 public class Maffs {
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+    public float add(float a, float b) {
+        return a + b;
+    }
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+    public float divide(float a, float b) {
+        return a/b;
+    }
+    
 }
