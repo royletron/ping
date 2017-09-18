43ada73c35d0523891e42e7ad89ec7503d72f0c2
Darren Royle <darren@diffblue.com>
2017-09-18 15:21:49 +0100

    Added pong and proper string import

---
+++ src/main/java/com/diffblue/ping/Maffs.java
@@ -0,0 +1,7 @@
+package com.diffblue.ping;
+
+/**
+ * Created by darrenroyle on 18/09/2017.
+ */
+public class Maffs {
+}
--- src/main/java/com/diffblue/ping/Ping.java
+++ src/main/java/com/diffblue/ping/Ping.java
@@ -1,11 +1,20 @@
 package com.diffblue.ping;
 
+import java.lang.String;
+
 public class Ping {
     public String checkPing(String inp) {
         if(inp.equals("ping")) {
             return "pong";
         } else {
             return "ping";
+        }
+    }
+    public String checkPong(String inp) {
+        if(inp.equals("pong")) {
+            return "ping";
+        } else {
+            return "pong";
         }
     }
 }
