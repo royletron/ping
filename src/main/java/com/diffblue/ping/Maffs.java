8f7a0d2907f871bd01a340fa1085fc6ae62c8a56
Darren Royle <darren@diffblue.com>
2017-09-18 15:24:39 +0100

    More maffs helps make less gaffs

--- src/main/java/com/diffblue/ping/Maffs.java
+++ src/main/java/com/diffblue/ping/Maffs.java
@@ -16,5 +16,10 @@ public class Maffs {
     public float divide(float a, float b) {
         return a/b;
     }
-    
+    public boolean isEven(float a) {
+        return a%2 == 0;
+    }
+    public boolean isOdd(float a) {
+        return a%2 == 1;
+    }
 }
